6dc53a9b1a2552a413e2d33b575d7ab63b012f08
nimmakayalaharitha <168528786+nimmakayalaharitha@users.noreply.github.com>
2025-06-17 19:36:51 +0530

    java

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Array {
+
+	public static void main(String[] args) {
+		ArrayList<String> a=new ArrayList<String>();
+		a.add("mani");
+		a.add("nani");
+		a.add("ani");
+		a.add("hani");
+		a.add("manu");
+		a.add(0, "hello");
+	System.out.println(a);
+	}
+
+}
